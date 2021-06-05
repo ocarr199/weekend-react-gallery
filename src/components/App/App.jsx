86093f6ef720f35e4872bf8c4c142cde1ac9378a
Owen Carruth <ocarr199@gmail.com>
2021-06-04 19:38:51 -0500

    added photos and setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import './App.css';
-
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList'
 function App() {
+
+  useEffect(()=> {
+  // first step  Render Data on Page Load
+    getGalleryList()
+}, [])
+
+const[galleryList, setGalleryList] = useState([])
+
+// Ran on page Load...
+const getGalleryList = ()=> {
+    console.log('get triggered')
+  //  finds get request in the server
+    axios.get('/gallery').then(response => {
+        // 6. save the data in the shoppingList state
+        setGalleryList(response.data)
+    }).catch(err => {
+        console.error(err)
+    })
+} 
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
+        <GalleryList />
         <img src="images/goat_small.jpg"/>
       </div>
     );
